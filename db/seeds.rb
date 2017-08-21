f4eff17dd012558ec678276e1f11bedbb496c5d7
Maxime Boue <abriko@yahoo.fr>
2017-08-21 18:48:01 +0200

    Working on seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,7 +30,8 @@ Category.create(name: "Écologie")
 tree = Tree.create(
   name: "Développeur Ruby",
   user_id: User.first.id,
-  categorie_id: Category.first.id)
+  categorie_id: Category.first.id
+  )
   url = "https://res.cloudinary.com/drua6zxss/image/upload/v1502884951/doll7_yh8aex_cr8dte.jpg"
   tree.photo_url = url
 
@@ -38,7 +39,8 @@ tree = Tree.create(
 tree = Tree.create(
   name: "Adepte écologie",
   user_id: User.last.id,
-  categorie_id: Category.last.id)
+  categorie_id: Category.last.id
+  )
   url = "https://res.cloudinary.com/drua6zxss/image/upload/v1502884951/doll7_yh8aex_cr8dte.jpg"
   tree.photo_url = url
 
@@ -46,7 +48,8 @@ tree = Tree.create(
 tree = Tree.create(
   name: "UX design",
   user_id: User.first.id,
-  categorie_id: Category.find(5))
+  categorie_id: Category.find(5)
+  )
   url = "https://res.cloudinary.com/drua6zxss/image/upload/v1502884951/doll7_yh8aex_cr8dte.jpg"
   tree.photo_url = url
 
@@ -54,9 +57,122 @@ tree = Tree.create(
 tree = Tree.create(
   name: "Développeur Ruby",
   user_id: User.first.id,
-  categorie_id: Category.find(8))
+  categorie_id: Category.find(8)
+  )
   url = "https://res.cloudinary.com/drua6zxss/image/upload/v1502884951/doll7_yh8aex_cr8dte.jpg"
   tree.photo_url = url
 
+skills_ruby = ["Command line basics", "Variables and assigments", "Build-in objects", "Controlling the flow", "Enumerables", "Array", "Methods", "Iteration", ]
 
-skill =
+15.times do
+
+skill = Skill.create(
+  title: "Command line basics",
+  tree_id: 1,
+  level: true,
+  )
+skill = Skill.create(
+  title: "Variables and assigment",
+  tree_id: 1,
+  level: true,
+  )
+skill = Skill.create(
+  title: "Build-in objects",
+  tree_id: 1,
+  level: true,
+  )
+skill = Skill.create(
+  title: "Controlling the flow",
+  tree_id: 1,
+  level: true,
+  )
+skill = Skill.create(
+  title: "Enumerables",
+  tree_id: 1,
+  level: true,
+  )
+skill = Skill.create(
+  title: "Array",
+  tree_id: 1,
+  level: true,
+  )
+skill = Skill.create(
+  title: "Methods",
+  tree_id: 1,
+  level: true,
+  )
+skill = Skill.create(
+  title: "Iteration",
+  tree_id: 1,
+  level: true,
+  )
+skill = Skill.create(
+  title: "Defining method",
+  tree_id: 1,
+  level: true,
+  )
+skill = Skill.create(
+  title: "Blocks",
+  tree_id: 1,
+  level: true,
+  )
+skill = Skill.create(
+  title: "Command line basics",
+  tree_id: 1,
+  level: true,
+  )
+skill = Skill.create(
+  title: "Command line basics",
+  tree_id: 1,
+  level: true,
+  )
+skill = Skill.create(
+  title: "Command line basics",
+  tree_id: 1,
+  level: true,
+  )
+skill = Skill.create(
+  title: "Command line basics",
+  tree_id: 1,
+  level: true,
+  )
+skill = Skill.create(
+  title: "Command line basics",
+  tree_id: 1,
+  level: true,
+  )
+skill = Skill.create(
+  title: "Command line basics",
+  tree_id: 1,
+  level: true,
+  )
+skill = Skill.create(
+  title: "Command line basics",
+  tree_id: 1,
+  level: true,
+  )
+skill = Skill.create(
+  title: "Command line basics",
+  tree_id: 1,
+  level: true,
+  )
+skill = Skill.create(
+  title: "Command line basics",
+  tree_id: 1,
+  level: true,
+  )
+skill = Skill.create(
+  title: "Command line basics",
+  tree_id: 1,
+  level: true,
+  )
+skill = Skill.create(
+  title: "Command line basics",
+  tree_id: 1,
+  level: true,
+  )
+skill = Skill.create(
+  title: "Command line basics",
+  tree_id: 1,
+  level: true,
+  )
