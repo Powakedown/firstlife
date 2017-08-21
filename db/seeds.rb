ad43a1210dc1a4c2dfab826d1e61f4a13b8dd471
Maxime Boue <abriko@yahoo.fr>
2017-08-21 18:30:35 +0200

    Working on seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,58 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+Tree.destroy_all
+Skill.destroy_all
+Category.destroy_all
+
+User.create(email: "doodlid@yahoo.fr", password: "123456", address: "Bordeaux")
+User.create(email: "etienne.ziegelmeyer@yahoo.fr", password: "123456", address: "Colmart")
+
+Category.create(name: "Développement Web")
+Category.create(name: "Graphisme")
+Category.create(name: "Artisanat")
+Category.create(name: "Spectacle")
+Category.create(name: "Design")
+Category.create(name: "Informatique d'entreprise")
+Category.create(name: "Développement personnel")
+Category.create(name: "Éducation")
+Category.create(name: "Tourisme")
+Category.create(name: "Écologie")
+
+
+
+tree = Tree.create(
+  name: "Développeur Ruby",
+  user_id: User.first.id,
+  categorie_id: Category.first.id)
+  url = "https://res.cloudinary.com/drua6zxss/image/upload/v1502884951/doll7_yh8aex_cr8dte.jpg"
+  tree.photo_url = url
+
+
+tree = Tree.create(
+  name: "Adepte écologie",
+  user_id: User.last.id,
+  categorie_id: Category.last.id)
+  url = "https://res.cloudinary.com/drua6zxss/image/upload/v1502884951/doll7_yh8aex_cr8dte.jpg"
+  tree.photo_url = url
+
+
+tree = Tree.create(
+  name: "UX design",
+  user_id: User.first.id,
+  categorie_id: Category.find(5))
+  url = "https://res.cloudinary.com/drua6zxss/image/upload/v1502884951/doll7_yh8aex_cr8dte.jpg"
+  tree.photo_url = url
+
+
+tree = Tree.create(
+  name: "Développeur Ruby",
+  user_id: User.first.id,
+  categorie_id: Category.find(8))
+  url = "https://res.cloudinary.com/drua6zxss/image/upload/v1502884951/doll7_yh8aex_cr8dte.jpg"
+  tree.photo_url = url
+
+
+skill =
