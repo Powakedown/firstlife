c37f8fd628f40c3eba1d6012462b6fa698916f43
Maxime Boue <abriko@yahoo.fr>
2017-08-22 14:07:02 +0200

    Fix seed with ancestry

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,3 +1,4 @@
 class Skill < ApplicationRecord
   belongs_to :tree
+  has_ancestry
 end
--- db/migrate/20170822103413_add_ancestry_to_skill.rb
+++ db/migrate/20170822103413_add_ancestry_to_skill.rb
@@ -1,5 +1,6 @@
 class AddAncestryToSkill < ActiveRecord::Migration[5.1]
   def change
     add_column :skills, :ancestry, :string
+    add_index :skills, :ancestry
   end
 end
