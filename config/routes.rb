3e648fa8067fbf45dfbc311f308607712aba63a3
Maxime Boue <abriko@yahoo.fr>
2017-08-22 08:55:55 +0200

    Add controller tree and index actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'trees/index'
+
+  get 'trees/show'
+
+  get 'trees/create'
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
