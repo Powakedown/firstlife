aaa2857fefaa275a5fd3814bbd998d34a401c01d
fouadpy <perceval@protonmail.com>
2017-08-28 15:44:06 +0200

    title search in tree/index and new element in navbar

--- app/models/tree.rb
+++ app/models/tree.rb
@@ -7,4 +7,9 @@ class Tree < ApplicationRecord
   validates :name, presence: true
   validates_uniqueness_of :name
 
+
+  def self.search(search)
+    where("name ILIKE ?", "%#{params[:query][:name]}%")
+  end
+
 end
