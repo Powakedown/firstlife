b8fd01474b03606b9cf1eeed930e544aea009d6a
Maxime Boue <abriko@yahoo.fr>
2017-08-28 14:15:04 +0200

    Do search function from home to user index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,22 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
+
+    if params[:query]
+      @query = params[:query][:name]
+      @address = params[:query][:address]
+      @trees = Tree.where("name ILIKE ?", "%#{@query}%")
+      @users = User.where("address ILIKE ?", "%#{params[:query][:address]}%")
+      @search = true
+    else
+      @trees = Tree.all
+      @users = User.all
+    end
+
+    @result = 0
+    @users.each do |user|
+      @result += 1 if user.trees.where("name ILIKE ?", "%#{@query}%").count > 0
+    end
   end
 
   def show
