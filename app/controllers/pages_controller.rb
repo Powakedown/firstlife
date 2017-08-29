5d5506c265fc8755275064717dee67b5b586a60b
Maxime Boue <abriko@yahoo.fr>
2017-08-29 12:25:42 +0200

    Work on users search

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,19 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    if params[:query]
-      @trees = Tree.where("name ILIKE ?", "%#{params[:query][:name]}%")
-      @search = true
-    else
-      @trees = Tree.where.not("name = ? OR name = ?", "Écologie et progrès", "Nature et écologie")
-      @users = User.all
-    end
-    @result = @trees.count
-    @tree = Tree.first
-  end
-
-  def index
-
-
+    @trees = Tree.where.not("name = ? OR name = ?", "Écologie et progrès", "Nature et écologie")
+    @users = User.all
   end
 end
