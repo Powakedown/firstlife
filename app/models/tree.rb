0ffa0ae38d773d649fe98b5e06545e11185cc88e
Maxime Boue <abriko@yahoo.fr>
2017-08-30 11:03:08 +0200

    Improve seed and models 2

--- app/models/tree.rb
+++ app/models/tree.rb
@@ -1,7 +1,7 @@
 class Tree < ApplicationRecord
   has_attachment :photo
-  has_many :skills
-  has_many :user_trees
+  has_many :skills, dependent: :destroy
+  has_many :user_trees, dependent: :destroy
   has_many :users, through: :user_trees
   belongs_to :category
   validates :name, presence: true
